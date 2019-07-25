a7cb3892db627cb033c489463c972200f475977a
Matt Mastin <49283941+mmastin@users.noreply.github.com>
2019-07-25 17:56:01 -0400

    First attempt

---
+++ web_iris_model_app.py
@@ -0,0 +1,34 @@
+# imports
+import os
+import numpy as np
+import flask
+import pickle
+from flask import Flask, render_template, request
+
+# pred logic
+def ValuePredictor():
+    to_predict = np.array(to_predict_list).reshape(1, -1)
+    loaded_model = pickle.load(open('iris_model.pkl', 'rb'))
+    result = loaded_model.predict(to_predict)
+    return result[0]
+
+# app
+app = Flask(__name__)
+
+# routes
+@app.route('/')
+@app.route('/index')
+def index():
+    return flask.render_template('index.html')
+@app.route('/result', methods = ['POST'])
+def result():
+    if request.method == 'POST':
+        to_predict_list = request.form.to_dict()
+        to_predict_list = list(to_predict_list.values())
+        to_predict_list = list(map(int, to_predict_list))
+        result = ValuePredictor(to_predict_list)
+    return render_template('results.html', predictions=result)
+
+# app run
+if __name__ == '__main__':
+    app.run(port=9000, debug=True)
